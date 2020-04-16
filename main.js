5f60238e0108c69b7956112807da4a6c067e1b51
AntonioGC <AntonioGC@uma.es>
2020-04-16 22:20:36 +0200

    Completamente funcional

--- main.js
+++ main.js
@@ -1,21 +1,81 @@
-b = false;
+jugador = 'X';
+
+tablero = []
+
+terminado = false;
 
 function marcar(c)
 {
-    boton = document.querySelector('#'+ c);
+    boton = document.querySelector('#b'+ c);
 
-    if(b)
+    if(boton.innerHTML != '' || terminado) return;
+
+    boton.innerHTML = jugador;
+    boton.className = 'casilla ' + jugador;
+
+    tablero[c - 1] = jugador;
+
+    if(comprobar())
     {
-        boton.innerHTML = 'X';
-        boton.className = 'casillaX';
+        terminado = true;        
     }
     else
     {
-        boton.innerHTML = 'O';
-        boton.className = 'casillaO';
+        cambiarJuagador();
+    }
+}
+
+function comprobar()
+{
+    for (let i = 0; i < 3; i++) {
+
+        fila = true;
+        columna = true;
+
+        for (let j = 0; j < 3; j++) {
+            fila = fila && (tablero[i + 3*j] == jugador);
+            columna = columna &&  (tablero[j + 3*i] == jugador);
+        }
+
+        if(fila || columna)
+        {
+            info = document.querySelector('#informacion');
+            info.innerHTML = 'El ganador es: ';
+            return true;
+        }
+    }
+    diag1 = true;
+    diag2 = true;
+
+    for (let i = 0; i < 3; i++) {
+        diag1 = diag1 && (tablero[i + 3*i] == jugador);
+        diag2 = diag2 && (tablero[(2-i) + 3*i] == jugador);
     }
 
-    b = !b;
+    if(diag1 || diag2)
+    {
+        info = document.querySelector('#informacion');
+        info.innerHTML = 'El ganador es: ';
+        return true;
+    }
+
+    return false;
+}
+
+function cambiarJuagador()
+{
+    if(jugador == 'X')
+    {
+        jugador = 'O';
+    }
+    else
+    {
+        jugador = 'X';
+    }
+
+    info = document.querySelector('#jugador');
+    info.innerHTML = jugador;
+    info.className = jugador;
 }
 
 function reiniciar()
@@ -25,5 +85,13 @@ function reiniciar()
         boton = document.querySelector('#b' + i);
         boton.innerHTML = '';
         boton.className = 'casilla';
+        tablero[i-1] = '';
     }
+    info = document.querySelector('#informacion');
+    info.innerHTML = 'Turno de: ';
+    j = document.querySelector('#jugador');
+    jugador = 'X';
+    j.innerHTML = jugador;
+    j.className = jugador; 
+    terminado = false;   
 }
